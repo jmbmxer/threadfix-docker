06a0baf939f86dda1ebe9df37f122fdb65a2ef1c
Jimmy Mesta <jmesta@invoca.com>
2016-09-09 12:08:25 -0700

    restructure dockerfile

--- scripts/build_images.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dir=$(dirname "$0")
-for image_dir in $dir/../images/*
-do
-  image_name=$(basename $image_dir)
-  echo "Building threadfix/$image_name"
-  (cd $image_dir && docker build -t threadfix/$image_name .)
-done
